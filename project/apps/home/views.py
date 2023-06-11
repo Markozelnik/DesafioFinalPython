99fbc73e32e86fc090dc483e676fde2934ff5624
Marcos Kozelnik <makozelnik25@gmail.com>
2023-06-11 20:04:54 -0300

    template 1

--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from . import forms
 
 # Create your views here.
+
+from . import forms
+
+
+def index(request):
+    return render(request, "home/index.html")
