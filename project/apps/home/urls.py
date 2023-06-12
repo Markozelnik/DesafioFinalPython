d567fe833025c485ca2368041c109e3f981f215c
Marcos Kozelnik <makozelnik25@gmail.com>
2023-06-11 21:57:06 -0300

    template 1

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    #path("crear-autor/", views.crear_autor, name="crear-autor"),
-    #path("crear-post/", views.crear_post, name="crear-post"),
+    path("crear-autor/", views.crear_autor, name="crear-autor"),
+    path("crear-post/", views.crear_post, name="crear-post"),
 ]
